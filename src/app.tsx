26165bfbeca889c7205197faea47a1aad6a09b56
jpedroccruz <joaopedroccruz.dev@gmail.com>
2026-09-19 19:19:48 -0300

    feat: add root page

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Sudo from "./pages/(guest)/sudo/page"
+import Root from "./pages/(private)/root/page"
 import Home from "./pages/(public)/home/page"
 
 const client = new QueryClient()
@@ -12,6 +13,7 @@ export default function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/sudo" element={<Sudo />} />
+					<Route path="/root" element={<Root />} />
 				</Routes>
 			</BrowserRouter>
 		</QueryClientProvider>
--- src/pages/(private)/root/page.tsx
+++ src/pages/(private)/root/page.tsx
@@ -1,3 +1,30 @@
+import { ArrowLeft, ArrowUpRight, CircleX, Plus } from "lucide-react"
+import Footer from "../../../components/footer"
+import useProject from "../../../hooks/use-project"
+import useStack from "../../../hooks/use-stacks"
+import Projects from "../../(public)/home/components/projects"
+import Stacks from "../../(public)/home/components/stacks"
+
 export default function Root() {
-	return <h1>Hello Root</h1>
+	const { data: projects } = useProject()
+	const { data: stacks } = useStack()
+
+	return (
+		<div className="flex min-h-screen flex-col bg-[#101010] text-[#f3f3f3] selection:bg-white selection:text-black">
+			<main className="mx-auto w-full flex max-w-240 flex-col gap-12 px-7 py-8 md:gap-20 md:px-0 md:py-12">
+				<a
+					className="flex w-fit items-center gap-2 text-[18px] text-[#909090] transition-colors hover:text-white"
+					href="/"
+				>
+					<ArrowLeft size={20} />
+					<span>return {"<Home/>"}</span>
+				</a>
+
+				<Stacks stacks={stacks ?? null} showAddButton />
+				<Projects projects={projects ?? null} showAddButton />
+			</main>
+
+			<Footer />
+		</div>
+	)
 }
